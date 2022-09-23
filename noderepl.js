850fc50f2000bb17b1e8135fe2bdffd3b204ecd1
Fernando Basso <fernandobasso.br@gmail.com>
2022-09-23 07:34:26 -0300

    Add script to run custom node REPL
    Create a custom node REPL script so we have some global helper functions
    easily available on every REPL session.
    
    Run as:
    
        $ node ./noderepl.js
    
    And exist the session with `Ctrl+D` or `.exit`.

--- noderepl.js
+++ noderepl.js
@@ -1,4 +1,17 @@
+//
+// Custom node repl script. Start it with:
+//
+//     $ node ./noderepl.js
+//
+// Exit with `Ctrl+D` or `.exit`.
+//
+
 import repl from 'repl';
+import {
+  statSync,
+  readFileSync,
+  appendFileSync,
+} from 'fs';
 
 [
   'log',
@@ -13,4 +26,33 @@ import repl from 'repl';
   global[fn] = Math[fn].bind(Math);
 });
 
-repl.start();
+global.log = console.log.bind(console);
+
+//
+// Save repl server object for later use.
+//
+const server = repl.start();
+
+const HISTFILE = './.node_repl_history';
+
+statSync(HISTFILE);
+
+//
+// Read history file into repl server history.
+//
+readFileSync(HISTFILE, 'UTF-8')
+  .split('\n')
+  .reverse()
+  .filter(line => line.trim())
+  .map(line => server.history.push(line))
+
+//
+// There must be a trailing '\n' so the next time we add
+// lines to the history we do not get the last command of
+// the previous sessions on the same line as the first command
+// of the next session.
+//
+server.on('exit', function onReplExit() {
+  appendFileSync(HISTFILE, server.lines.join('\n') + '\n');
+  process.exit();
+});
