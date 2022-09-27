7e0364b4716374d5c9dbb78ce69b504bc846f31b
Fernando Basso <fernandobasso.br@gmail.com>
2022-09-27 09:21:19 -0300

    Add another multiple pointer approach example

--- src/05-problem-solving-patterns/countUniqVals.test.ts
+++ src/05-problem-solving-patterns/countUniqVals.test.ts
@@ -1,17 +1,18 @@
-import { countUniqVals } from './countUniqVals-v2';
+import { assertEquals } from "/deps.ts";
+import { countUniqVals } from './countUniqVals-v1.ts';
 
-describe('countUniqVals()', () => {
-  it('should return 0 with empty input', () => {
-    expect(countUniqVals([])).toEqual(0);
-    expect(countUniqVals(new Array(0))).toEqual(0);
+Deno.test('countUniqVals()', async (t) => {
+  await t.step('should return 0 with empty input', () => {
+    assertEquals(countUniqVals([]), 0);
+    assertEquals(countUniqVals(new Array(0)), 0);
   });
 
-  it('should return the correct count of unique values', () => {
-    expect(countUniqVals([0])).toEqual(1);
-    expect(countUniqVals([-7])).toEqual(1);
-    expect(countUniqVals([-3, -3, -3])).toEqual(1);
-    expect(countUniqVals([-3, -3, 0, 1, 1, 1, 1, 1])).toEqual(3);
-    expect(countUniqVals([-3, -2, -1, 0, 0, 1, 1])).toEqual(5);
-    expect(countUniqVals([-3, -2, -1, 0, 1])).toEqual(5);
+  await t.step('should return the correct count of unique values', () => {
+    assertEquals(countUniqVals([0]), 1);
+    assertEquals(countUniqVals([-7]), 1);
+    assertEquals(countUniqVals([-3, -3, -3]), 1);
+    assertEquals(countUniqVals([-3, -3, 0, 1, 1, 1, 1, 1]), 3);
+    assertEquals(countUniqVals([-3, -2, -1, 0, 0, 1, 1]), 5);
+    assertEquals(countUniqVals([-3, -2, -1, 0, 1]), 5);
   });
 });
