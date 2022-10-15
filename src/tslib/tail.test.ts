1cbd24b4bb76333adfaa7f1f704f61b023b2a410
Fernando Basso <fernandobasso.br@gmail.com>
2022-10-15 11:15:06 -0300

    Update tail() with better types

--- src/tslib/tail.test.ts
+++ src/tslib/tail.test.ts
@@ -6,41 +6,48 @@ Deno.test("tail()", async (t) => {
     assertThrows(
       () => tail([]),
       RangeError,
-      "tail(): array cannot be empty."
+      "tail(): input cannot be empty."
+    );
+
+    assertThrows(
+      () => tail(""),
+      RangeError,
+      "tail(): input cannot be empty."
     );
   });
 
-  await t.step("when the array contains a single element", async (t) => {
-    await t.step("should return an empty array", () => {
-      assertEquals(tail([0]), []);
-      assertEquals(tail(["hello"]), []);
-    });
+  await t.step("should return an the unmodified empty tail", () => {
+    assertEquals(tail([0]), []);
+    assertEquals(tail(["hello"]), []);
+    assertEquals(tail("Z"), "");
   });
 
-  await t.step("when array contains two or more elements", async (t) => {
-    await t.step("should return the the array with the first element dropped", () => {
-      assertEquals(tail([0, 1]), [1]);
-      assertEquals(tail([0, 1, 2, 3, 4]), [1, 2, 3, 4]);
+  await t.step("should return the the input with the first element dropped", () => {
+    assertEquals(tail([0, 1]), [1]);
+    assertEquals(tail([0, 1, 2, 3, 4]), [1, 2, 3, 4]);
 
-      assertEquals(tail(["hello", "world"]), ["world"]);
-      assertEquals(
-        tail(["May", "the", "force", "be", "with", "you."]),
-        ["the", "force", "be", "with", "you."],
-      );
+    assertEquals(tail("hello"), "ello");
 
-      assertEquals(
-        tail([[0, "zero"], [1, "one"], [2, "two"]]),
-        [[1, "one"], [2, "two"]]
-      );
+    assertEquals(tail(["hello", "world"]), ["world"]);
+    assertEquals(
+      tail(["May", "the", "force", "be", "with", "you."]),
+      ["the", "force", "be", "with", "you."],
+    );
 
-      assertEquals(
-        tail([
-          { id: 1, name: "Yoda" },
-          { id: 2, name: "Aayla Secura" },
-        ]),
-        [{ id: 2, name: "Aayla Secura" }],
-      );
-    });
+    assertEquals(
+      tail([[0, "zero"], [1, "one"], [2, "two"]]),
+      [[1, "one"], [2, "two"]]
+    );
+
+    assertEquals(
+      tail([
+        { id: 1, name: "Yoda" },
+        { id: 2, name: "Aayla Secura" },
+      ]),
+      [{ id: 2, name: "Aayla Secura" }],
+    );
+
+    assertEquals(tail("✔λ✗"), "λ✗");
   });
 });
 
--- src/tslib/tail.ts
+++ src/tslib/tail.ts
@@ -1,15 +1,24 @@
 /**
- * Returns the array without its first element. Throws an exception if
- * the array is empty.
+ * Returns the input without its first element. Throws an exception if
+ * the input is empty.
  *
  * Lisps and Schemes name this function `cdr`. Some other languages
  * call it `rest`.
  *
  * @sig Array<T> -> T | Never
  */
-export function tail<T>(xs: T[]): T[] | never {
+export function tail(xs: ""): never;
+export function tail(xs: readonly []): never;
+export function tail(xs: string): string;
+export function tail<T>(xs: T[]): T[];
+export function tail<T>(
+  xs: "" | readonly T[] | T[] | string,
+): T | string | never;
+export function tail<T>(
+  xs: "" | [] | readonly T[] | string,
+): T[] | string | never {
   if (xs.length === 0)
-    throw new RangeError("tail(): array cannot be empty.");
+    throw new RangeError("tail(): input cannot be empty.");
 
   return xs.slice(1);
 }
