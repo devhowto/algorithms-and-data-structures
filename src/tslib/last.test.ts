ed855319dda2cbd9d416aa638e4d46cf20a85a3f
Fernando Basso <fernandobasso.br@gmail.com>
2022-10-15 14:54:39 -0300

    Update last() to also handle strings

--- src/tslib/last.test.ts
+++ src/tslib/last.test.ts
@@ -2,18 +2,26 @@ import { assertEquals, assertThrows } from "/deps.ts";
 import { last } from "./index.ts";
 
 Deno.test("last()", async (t) => {
-  await t.step("should throw for empty array", () => {
+  await t.step("empty input", () => {
     assertThrows(
       () => last([]),
       RangeError,
-      "last(): array cannot be empty."
+      "last(): input cannot be empty."
+    );
+
+    assertThrows(
+      () => last(""),
+      RangeError,
+      "last(): input cannot be empty."
     );
   });
 
-  await t.step("should return last element", () => {
+  await t.step("non-empty input", () => {
     assertEquals(last([Infinity]), Infinity);
     assertEquals(last([1e1, 1e2, -1e3]), -1e3);
     assertEquals(last(["may", "the", "force"]), "force");
     assertEquals(last(["ø", "‽", "✔", "✗", "λ", "💩"]), "💩");
+    assertEquals(last("z"), "z");
+    assertEquals(last("Check ✔"), "✔");
   });
 });
--- src/tslib/last.ts
+++ src/tslib/last.ts
@@ -1,12 +1,22 @@
+import {
+  isEmpty,
+} from "/src/tslib/index.ts";
+
 /**
  * Returns the last element of an array. Throws an exception if the
  * array is empty.
  *
  * @sig Array<T> -> T | Never
  */
-export function last<T>(xs: T[]): T | never {
-  if (xs.length === 0)
-    throw new RangeError("last(): array cannot be empty.");
+export function last(xs: ""): never;
+export function last(xs: readonly []): never;
+export function last(xs: string): string;
+export function last<T>(xs: readonly T[]): T;
+export function last<T>(
+  xs: "" | readonly [] | string | readonly T[]
+): T | string | never {
+  if (isEmpty(xs))
+    throw new RangeError("last(): input cannot be empty.");
 
   return xs[xs.length - 1];
 }
