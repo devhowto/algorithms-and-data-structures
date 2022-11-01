35db976919183f318d3fb1fbef2add37043d4ddf
Fernando Basso <fernandobasso.br@gmail.com>
2022-11-01 08:32:37 -0300

    Add selection sort notes and solutions

---
+++ src/12-selection-sort/sortAsc-v1.ts
@@ -0,0 +1,49 @@
+//
+// tags: algorithm sort selection-sort
+//
+
+/**
+ * Swaps the elements at `idx1` and `idx2`.
+ *
+ * NOTE: This function is impure. It mutates the passed array.
+ *
+ * @sig [a] Int Int -> [a]
+ */
+function swap(
+  xs: number[],
+  idx1: number,
+  idx2: number,
+): void {
+  [xs[idx1], xs[idx2]] = [xs[idx2], xs[idx1]];
+}
+
+/**
+ * Sorts an array in ascending order using the selection sort algorithm.
+ *
+ * This solution is OK as it keeps iterating on an ever shrinking
+ * portion of the input, so, it does not do unnecessary work (as far as
+ * selection sort goes).
+ *
+ * - T.C: O(n²).
+ * - S.C: O(n).
+ *
+ * @sig [Number] -> [Number]
+ */
+export function sortAsc(xs: number[]): number[] {
+  const len = xs.length;
+
+  for (let i = 0; i < len; ++i) {
+    const ini = i;
+    let min = i;
+
+    for (let j = i + 1; j < len; ++j) {
+      if (xs[j] < xs[min])
+        min = j;
+    }
+
+    if (xs[min] < xs[ini])
+      swap(xs, ini, min);
+  }
+
+  return xs;
+}
