fdf618dd51180d89aef535e1f00b62ba093e0f7b
Fernando Basso <fernandobasso.br@gmail.com>
2022-12-21 07:55:27 -0300

    WIP on BSTs

---
+++ js/binary-search-trees/BST.spec.js
@@ -0,0 +1,42 @@
+import {
+  Node,
+  BST,
+} from './BST.js';
+
+describe('Node', () => {
+  test('can create a Node', () => {
+    var node = new Node(100);
+    expect(node.val).toEqual(100);
+    expect(node.left).toBe(null);
+    expect(node.right).toBe(null);
+  });
+});
+
+describe('BST()', () => {
+  test('create an empty BST', () => {
+    var bst = new BST();
+
+    expect(bst.root).toBe(null);
+  });
+
+  test('insert to empty BST', () => {
+    var bst = new BST();
+    var res = bst.insert(100);
+
+    expect(res === bst).toBe(true);
+    expect(bst.root.val).toEqual(100);
+    expect(bst.root.left).toBe(null);
+    expect(bst.root.right).toBe(null);
+  });
+
+  test('insert to the left of single node BST', () => {
+    var bst = new BST().insert(100);
+
+    var res = bst.insert(40);
+
+    expect(res === bst).toBe(true);
+    expect(bst.root.left.val).toEqual(40);
+    expect(bst.root.right).toBe(null);
+  });
+});
+
