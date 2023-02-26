15985a37bdf99cff5952fc37274085eadbd4eb12
Fernando Basso <fernandobasso.br@gmail.com>
2023-02-26 12:07:57 -0300

    rb: Study different meetup dates solution

--- exercism/ruby/meetup/meetup.rb
+++ exercism/ruby/meetup/meetup.rb
@@ -1,65 +1,34 @@
+##
+# This solution is not mine. Got it here:
 #
-# tags: date, range
+# • https://exercism.org/tracks/ruby/exercises/meetup/solutions/kkchu791
 #
 
 require 'date'
 
 class Meetup
-  VERSION = 1
-
-  DAYS_OF_WEEK = {
-    sunday: 0,
-    monday: 1,
-    tuesday: 2,
-    wednesday: 3,
-    thursday: 4,
-    friday: 5,
-    saturday: 6,
-  }.freeze
-
-  START_DAYS = {
-    first: 1,
-    second: 8,
-    teenth: 13,
-    third: 15,
-    fourth: 22,
-    #
-    # We don't know when last starts. Last day of the month
-    # could be one of 28, 29, 30, or 31. We use `last_start_day`
-    # to help figure this out.
-    #
-  }.freeze
+  attr_reader :month, :year, :days_in_month, :calendar
 
   def initialize(month, year)
     @month = month
     @year = year
+    @days_in_month = (Date.new(year, 12, 31) << (12 - month)).day
+
+    @calendar = {
+      first: [*1..7],
+      second: [*8..14],
+      third: [*15..21],
+      fourth: [*22..28],
+      teenth: [*13..19],
+      last: [*(days_in_month - 6)..days_in_month],
+    }
   end
 
-  ##
-  # @param day_of_week [Symbol]
-  # @param nth [Integer]
-  #
-  def day(day_of_week, which)
-    date = Date.new(@year, @month, start_day(which))
+  def day(weekday, schedule)
+    section_of_month = calendar[schedule]
 
-    6.times do
-      break if date.wday == DAYS_OF_WEEK[day_of_week]
-
-      date = date.next_day
-    end
-
-    date
-  end
-
-  private
-
-  def last_start_day
-    Date.new(@year, @month).next_month - 7
-  end
-
-  def start_day(which)
-    return last_start_day.mday if which == :last
-
-    START_DAYS[which]
+    Date.new(@year, @month, section_of_month.select do |x|
+      Date.new(year, month, x).strftime('%A') == weekday.to_s.capitalize
+    end.join.to_i)
   end
 end
