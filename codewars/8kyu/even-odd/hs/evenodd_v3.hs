08db0c286ba90654bdddd7e5a597db0e36a1d39c
Fernando Basso <fernandobasso.br@gmail.com>
2023-04-25 08:22:56 -0300

    codewars(8kyu,hs): odd or even -> string

---
+++ codewars/8kyu/even-odd/hs/evenodd_v3.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+evenOrOdd :: Integral a => a -> String
+evenOrOdd n
+  | even n    = "Even"
+  | otherwise = "Odd"
+
+run :: IO ()
+run = do
+  putStrLn $ evenOrOdd (-1)
+  putStrLn $ evenOrOdd (3)
+  putStrLn $ evenOrOdd (4)
+  putStrLn $ evenOrOdd (-14)
+  putStrLn $ evenOrOdd (0)
+  putStrLn $ evenOrOdd (-0)
