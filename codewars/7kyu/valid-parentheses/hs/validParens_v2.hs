fe27ca6eb685572da926e84d911b25d83c585615
Fernando Basso <fernandobasso.br@gmail.com>
2023-06-12 08:55:33 -0300

    codewars(7kyu,hs): Refactor valid parentheses v2

--- codewars/7kyu/valid-parentheses/hs/validParens_v2.hs
+++ codewars/7kyu/valid-parentheses/hs/validParens_v2.hs
@@ -1,15 +1,14 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
 validParens :: String -> Bool
-validParens str = (== 0) count
-  where count = foldr
-                (\c n ->
-                    if n < 0 then n
-                    else if c == '('
-                         then n - 1
-                         else n + 1) 0 str
-
--- num = foldr (\c n -> if n
+validParens = (== 0) . foldr
+              (\c n ->
+                  if n < 0
+                  then n
+                  else
+                    if c == ')'
+                    then n + 1
+                    else n - 1) 0
 
 main :: IO ()
 main = do
@@ -22,3 +21,4 @@ main = do
   print $ validParens "((()))"          -- => True
   print $ validParens "())()("          -- => False
   print $ validParens "(())((()())())"  -- => True
+
