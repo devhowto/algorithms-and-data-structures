ffcb4aeebca73370803feb3d01bb234fa3d7410b
Fernando Basso <fernandobasso.br@gmail.com>
2023-06-12 09:26:04 -0300

    Update fibonacci haskell solution

--- other/fibonacci/hs/fib_v1.hs
+++ other/fibonacci/hs/fib_v1.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
 fib :: Integer -> [Integer]
-fib n = go n []
+fib n = go 0 []
   where
     go :: Integer -> [Integer] -> [Integer]
-    go i seq
-      | i == 0 = []
-      | i == 1 =
+    go i xs
+      | i == n = xs
+      | i == 0 = go (i + 1) [0]
+      | i == 1 = go (i + 1) [0, 1]
+      | otherwise = go (i + 1) (xs ++ [last xs + (last . init) xs])
