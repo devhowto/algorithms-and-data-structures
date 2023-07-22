3e3da0b946ecb659c130b0c73ec5bf37b6280632
Fernando Basso <fernandobasso.br@gmail.com>
2023-07-22 12:13:41 -0300

    hackerrank: Refactor grading students v1

--- hackerrank/easy/grading-students/js/grade_v1.js
+++ hackerrank/easy/grading-students/js/grade_v1.js
@@ -1,8 +1,13 @@
 /**
  * Given some integer `num`, find its next multiple of `multiplier`.
  *
- * ASSUME: Both values are integers.
+ * ASSUME: Both values are integers. If input are not integers, the
  *
+ * ASSUME: The multiplier is not zero, which would cause `NaN` to
+ * be returned.
+ *
+ * The result is always an integer, unless it returns `NaN` due to
+ * incorrect inputs.
  *
  * @param {number} num
  * @param {number} multiplier
@@ -12,6 +17,18 @@ function getNextMultOf(multiplier, num) {
   return Math.ceil(num / multiplier) * multiplier;
 }
 
+/**
+ * Conditionally rounds `grade` according to the rules.
+ *
+ * - If grade < 38, do nothing.
+ * - If grade >= 38, then:
+ *   - If difference between next multiple of the grade and the grade
+ *     is less than 3, round up to the next multiple of 5.
+ *   - Otherwise, do nothing.
+ *
+ * @param {number} grade
+ * @returns {number}
+ */
 function round(grade) {
   const newGrade = getNextMultOf(5, grade);
 
@@ -20,15 +37,18 @@ function round(grade) {
     : grade;
 }
 
+/**
+ * Computes the student grades according to the grading rules.
+ *
+ * @param {Array<number>} grades
+ * @returns {Array<number>}
+ */
 function gradeStudents(grades) {
   const newGrades = [];
+  let i = 0, grade;
 
-  for (let i = 0; i < grades.length; ++i) {
-  // for (let i = 1, grade = grades[i]; i < grades.length; ++i) {
-    const grade = grades[i];
-
+  while ((grade = grades[i++]) !== undefined)
     newGrades.push(grade < 38 ? grade : round(grade));
-  }
 
   return newGrades;
 }
