95627c35e47336ced8f3468b1d9753c26fcfc2b2
Fernando Basso <fernandobasso.br@gmail.com>
2023-08-20 07:39:55 -0300

    Add correct code of missing digits v1

--- other/missing-digit/js/missdig_v1.js
+++ other/missing-digit/js/missdig_v1.js
@@ -32,24 +32,41 @@ const invOpTbl = {
  */
 function misDig(str) {
   const [a, op, b, _, r] = str.split(' ');
-  log({ a, op, b, r });
   let num;
 
   if (r.includes('x')) {
     num = opTbl[op](int(a), int(b));
     return num.toString()[r.indexOf('x')];
-  } else if (a.includes('x')) {
-    num = opTbl[invOpTbl[op]](int(r), int(b));
-    return num.toString()[a.indexOf('x')];
-  } else {
+  } else if (b.includes('x') && ['-', '/'].includes(op)) {
+    num = opTbl[op](int(a), int(r));
+    return num.toString()[b.indexOf('x')];
+  } else if (b.includes('x')) {
     num = opTbl[invOpTbl[op]](int(r), int(a));
     return num.toString()[b.indexOf('x')];
+  } else {
+    num = opTbl[invOpTbl[op]](int(r), int(b));
+    return num.toString()[a.indexOf('x')];
   }
 }
 
-// log(misDig('2 * 3 = x')); // 5
-// log(misDig('x + 3 = 5')); // 2
-// log(misDig('2 + x = 5')); // 3
+log(misDig('2 + 3 = x')); // 5
+log(misDig('2 + x = 5')); // 3
+log(misDig('x + 3 = 5')); // 2
+log();
+
+log(misDig('5 - 4 = x')); // 1
+log(misDig('5 - x = 1')); // 4
+log(misDig('x - 4 = 1')); // 5
+log();
+
+log(misDig('2 * 4 = x')); // 8
+log(misDig('2 * x = 8')); // 4
+log(misDig('x * 4 = 8')); // 2
+log();
+
+log(misDig('8 / 4 = x')); // 2
+log(misDig('8 / x = 2')); // 4
+log(misDig('x / 4 = 2')); // 8
 
 // log(misDig('12x + 2 = 125')); // 3
 // //            3
@@ -60,7 +77,7 @@ function misDig(str) {
 //
 // log(misDig('5 - 2 = x')); // 3
 // log(misDig('x - 2 = 3')); // 5
-log(misDig('5 - x = 3')); // 2
+// log(misDig('5 - x = 3')); // 2
 
 /*
 5 - x  = 3
